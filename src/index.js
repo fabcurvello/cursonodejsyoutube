5761ea9c13e1c2281cec5024ba22d94b2c2e221e
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2026-07-22 09:30:49 -0300

    middleware

--- src/index.js
+++ src/index.js
@@ -4,17 +4,22 @@ const { v4: uuidv4 } = require("uuid");
 const app = express(); // constante que contém uma instância da dependência express
 app.use(express.json()); // usar json em todas as rotas
 
-const projects = ["a"];
+const projects = [ ];
 
-// criando a rota raiz -> GET http://localhost:3000/
-// request traz info sobre o cliente que requisitou esta rota. Response é a resposta dada ao cliente.
-// app.get("/", function (request, response) {
-//   // return response.send('Olá Dev! Bem vindo ao curso!');
-//   return response.json({
-//     message: "Olá Dev! Bem vindo ao curso!",
-//   });
-// });
+// Middleware - Recurso a ser executado entre a chegada e a saída da requisição
+function logRoutes(request, response, next) {
+  const { method, url } = request; // pegar method (método http) e url da request
+  const route = `[${method.toUpperCase()}] ${url}`;
+  console.log(route);
+  return next(); // próxima etapa, que é o que a requisição realmente quer fazer (get, post..)
+}
+// executar Middleware antes das próximas ações
+app.use(logRoutes); // execução para TODAS as ações 
 
+
+// Middleare: Se a necessidade for que o middleware roda em uma ação específica, 
+// chamr ele (ex: logRoutes) como abaixo, entre a rota e a function
+// app.get("/projects", logRoutes ,function (request, response) {
 app.get("/projects", function (request, response) {
   return response.json(projects);
 });
@@ -40,6 +45,7 @@ app.put("/projects/:id", function (request, response) {
   const { name, owner } = request.body;
 
   const projectIndex = projects.findIndex((p) => p.id === id);
+
   if (projectIndex < 0) {
     return response.status(404).json({ error: "Project not found" });
   }
@@ -60,7 +66,17 @@ app.put("/projects/:id", function (request, response) {
 });
 
 app.delete("/projects/:id", function (request, response) {
-  return response.json(["Projeto 2", "Projeto 3"]);
+  const { id } = request.params;
+  const projectIndex = projects.findIndex((p) => p.id === id);
+  
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: "Project not found" });
+  }
+
+  // splice para apagar do array. Posição inicial do array, quantos itens a apagar a partir da posição inicial.
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 //app.listen(3000); // porta que será utilizada pelo servidor.
